b07930f765dc6aad9b18294f75c174529b36e16d
wuyumax <332062922@qq.com>
2022-03-11 19:01:45 +0800

    update project name

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r",encoding='utf-8') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="TaskSpider",
+  version="0.0.1",
+  author="WuYuMax",
+  author_email="332062922@qq.com",
+  description="A framework which can help you make your webspider more OOP and easy",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/WuYuMax/EasySpider",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: Apache Software License",
+  "Operating System :: OS Independent",
+  ],
+)
