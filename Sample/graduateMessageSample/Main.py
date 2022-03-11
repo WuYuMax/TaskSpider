707d24d1a1ce43324b5464a794b3fb3eabb28f80
Dragon <332062922@qq.com>
2022-03-11 16:09:52 +0800

    修改了一下文件格式:

--- Sample/graduateMessageSample/Main.py
+++ Sample/graduateMessageSample/Main.py
@@ -6,8 +6,8 @@ from Task.TaskMessage import TaskMessage
 if __name__ == '__main__':
 
     ss = ''
-    ml = 'zyxw'
-    zy = '电子信息'
+    ml = ''
+    zy = '法学'
     file_path = './dic/'+ml+zy+'.csv'
 
     GraduateMessageTask(file_path,ss,ml,zy).run(TaskMessage())
